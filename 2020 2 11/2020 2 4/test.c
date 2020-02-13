f3e68d7f159cf6a573e5e3b5f7928223f39b4878
NS-zhao <1783583968@qq.com>
2020-02-13 12:13:49 +0800

    演示

---
+++ 2020 2 11/2020 2 4/test.c
@@ -0,0 +1,120 @@
+#include<stdio.h>
+//int main()
+//{
+//	printf("hello\n");
+//	printf("he he\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("%d\n", sizeof(char));                 1
+//	printf("%d\n", sizeof(short));                2
+//	printf("%d\n", sizeof(int));                  4
+//	printf("%d\n", sizeof(long));                 4
+//	printf("%d\n", sizeof(long long));            8 
+//	printf("%d\n", sizeof(float));                4
+//	printf("%d\n", sizeof(double));               8
+//	printf("%d\n", sizeof(long double));          8
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	printf("%d\n", 100);
+//	printf("%c\n", 100);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	short age = 20;
+//	//float weight = 55.6;
+//	float weight = 55.6f;
+//	printf("age = %d\n", age);
+//    printf("weight = %f\n", weight);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	double weight = 55.5;
+//	weight = 48.0;
+//	printf("%lf\n", weight);
+//	return 0;
+//}
+
+
+//int global = 2019;
+//int main()
+//{
+//	int local = 2018;
+//	int global = 2020;
+//	printf("global = %d\n", global);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("sum = %d\n", sum);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char arr[5] = { 0 };
+//	scanf("%s", arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//    int num = 10;
+//	num = 20;
+//	printf("num = %d\n", num);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	char arr1[20] = "abcdef";
+//	char arr2[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
+//	char arr3[] = { 'a', 'b', 'c', 'd', 'e', 'f' ,'\0'};
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//	printf("%s\n", arr3);
+//
+//	return 0;
+//}
+
+//#include<string.h>
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	printf("%d\n", strlen(arr));
+//	return 0;
+//}
+
+
