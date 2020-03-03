8b6e7ed20b7b85ce15ed53a4cbea6ae85cb9d786
NS-zhao <1783583968@qq.com>
2020-03-03 22:01:38 +0800

    复习

---
+++ 比特/2020 3 3/2020 3 3/test.c
@@ -0,0 +1,168 @@
+﻿
+//#include <stdio.h>
+//
+//
+//int main()
+//{
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//
+//	return 0;
+//}
+//
+//
+//
+//
+//int main()
+//{
+//	short age = 20;
+//	float weight = 5.6f;
+//	
+//	printf("age = %d\n", age);
+//	printf("weight = %f\n", weight);
+//
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long)); 
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//
+//	printf("%d\n", 100);
+//	printf("%c\n", 100);
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	double weight = 55.5;
+//	weight = 48.0;
+//	printf("%lf\n", weight);
+//
+//	return 0;
+//}
+//
+//int a = 100;
+//
+//
+//
+//int main()
+//{
+//	int a = 10;
+//	printf("a = %d\n", a);//
+//
+//	return 0;
+//}
+//
+//
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1+num2;
+//	printf("sum = %d\n", sum);
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	char arr[5] = {0};
+//
+//
+//	printf("%s\n", arr);
+//	return 0;
+//}
+//
+//int a = 10;
+//
+//void test()
+//{
+//	printf("a = %d\n", a);
+//}
+//int main()
+//{
+//	{
+//		printf("a = %d\n", a);
+//	}
+//	printf("a = %d\n", a);
+//	test();
+//	return 0;
+//}
+//
+//extern int g_val;
+//
+//int main()
+//{
+//	printf("g_val = %d\n", g_val);
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	
+//	{
+//		int a = 10;
+//	}
+//	printf("a = %d\n", a);
+//
+//	return 0;
+//}
+//
+//#define MAX 1000
+//
+//struct Stu
+//{
+//	char name[20];
+//	short age;
+//	char id[20];
+//};
+//
+//
+//
+//enum Sex
+//{
+//	MALE,
+//	FEMALE,
+//	SECRET
+//};
+//
+//int main()
+//{
+//	
+//	printf("%d\n", MALE);
+//	printf("%d\n", FEMALE);
+//	printf("%d\n", SECRET);
+//
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	
+//	char arr1[] = "abcdef";
+//	char arr2[] = {'a', 'b', 'c', 'd', 'e', 'f', 0};
+//
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//
+//	return 0;
+//}
+//#include <string.h>
+//
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	printf("%d\n", strlen(arr));
+//	return 0;
+//}
