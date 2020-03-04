ae721d564558f1260fed495ffff970d7a42e41f3
NS-zhao <1783583968@qq.com>
2020-03-04 22:28:27 +0800

    复习

---
+++ 比特/2020 3 4-2/2020 3 4-2/test.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+//struct Stu
+//{
+//	char name[20];
+//	short age;
+//	char sex[5];
+//	char id[15];
+//
+//};
+//int main()
+//{
+//	struct Stu zhangsan = { "张三", 20, "男", "2020214" };
+//	printf("%s %d %s %s\n", zhangsan.name, zhangsan.age, zhangsan.sex, zhangsan.id);
+//	return 0;
+//}
+
+
+
+//struct Stu
+//{
+//	char name[20];
+//	short age;
+//	char sex[5];
+//	char id[15];
+//
+//};
+//int main()
+//{
+//	struct Stu *pzs = NULL;
+//	
+//	struct Stu zhangsan = { "张三", 20, "男", "2020214" };
+//	pzs = &zhangsan;
+//	printf("%s %d %s %s\n", (*pzs).name,(*pzs).age,(*pzs).sex,(*pzs).id);
+//	printf("%s %d %s %s\n", pzs->name,pzs->age,pzs->sex,pzs->id);
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	printf("%d\n", sizeof(char *));
+//	printf("%d\n", sizeof(short *));
+//	printf("%d\n", sizeof(int *));
+//	printf("%d\n", sizeof(float *));
+//	printf("%d\n", sizeof(double *));
+//	printf("%d\n", sizeof(long *));
+//	printf("%d\n", sizeof(long long *));
+//	printf("%d\n", sizeof(long double *));
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//		if (i % 2 == 1)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i < 100; i+=2)
+//	{
+//		
+//			printf("%d ", i);
+//		
+//	}
+//	return 0;
+//}
