5ed2a8fd1422fd43249015017285934903fe1f96
NS-zhao <1783583968@qq.com>
2020-03-05 21:55:27 +0800

    复习

---
+++ 比特/2020 3 5/2020 3 5/test.c
@@ -0,0 +1,160 @@
+#include<stdio.h>
+
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		if (i == 5)
+//			break;
+//		printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+
+
+
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		i++;
+//
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
+//
+//	}
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//
+//		putchar(ch);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if (ch < '0' || ch > '9')
+//			continue;
+//		putchar(ch);
+//	}
+//		
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int ch = getchar();
+//	printf("%c\n", ch);
+//
+//	ch = getchar();
+//	printf("%c\n", ch);
+//	
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		printf("%d ", i);
+//
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	i = 1;
+//	while (i <= 10)
+//	{
+//		printf("hehe\n");
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if (i == 5)
+//			break;
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+
+
