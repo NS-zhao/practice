de1b9d6880d96dc067009d3ed3f55709688835a3
NS-zhao <1783583968@qq.com>
2020-03-09 23:21:10 +0800

    递归

---
+++ 比特/2020 3 9/2020 3 9/test.c
@@ -0,0 +1,178 @@
+#include<stdio.h>
+
+
+//void new_line()
+//{
+//	printf("hehe\n");
+//}
+//
+//void three_line()
+//{
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		new_line();
+//	}
+//}
+//
+//int main()
+//{
+//	
+//	three_line();
+//	return 0;
+//}
+
+
+
+//#include<string.h>
+//
+//int main()
+//{
+//	char arr[20] = "hello";
+//	int ret = strlen(strcat(arr, "bit"));
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+//#include <string.h>
+//
+//int main()
+//{
+//	char string[80];
+//	strcpy(string, "Hello world from ");
+//	strcat(string, "strcpy ");
+//	strcat(string, "and ");
+//	strcat(string, "strcat!");
+//	printf("String = %s\n", string);
+//	return 0;
+//}
+
+
+
+//#include<string.h>
+//int main()
+//{
+//	printf("%d\n", strlen("girl"));
+//	return 0;
+//}
+
+
+
+//#include<string.h>
+//int main()
+//{
+//	int len = strlen("girl");
+//	printf("%d\n",len);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("%d", printf("%d", printf("%d",43 )));
+//	return 0;
+//}
+
+
+
+//#include"add.h"
+//#include"sub.h"
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int ret = Add(a, b);
+//	printf("%d\n", ret);
+//	ret = Sub(3, 4);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	printf("hehe\n");
+//	main();
+//	return 0;
+//}
+
+
+
+
+//void Print(unsigned int num)
+//{
+//	if (num > 9)
+//	{
+//		Print(num / 10);
+//	}
+//	printf("%d ", num % 10);
+//}
+//
+//int main()
+//{
+//	unsigned int num = 1234;
+//	Print(num);
+//	return 0;
+//}
+
+
+
+//void test(int n)
+//{
+//	if (n <= 10000)
+//	{
+//		test(n + 1);
+//	}
+//}
+//
+//int main()
+//{
+//	test(1);
+//	return 0;
+//}
+
+
+
+//#include<string.h>
+//int my_strlen(char *str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//
+//int main()
+//{
+//	int len = my_strlen("girl");
+//	printf("len = %d\n", len);
+//	return 0;
+//}
+
+
+
+
+//#include<string.h>
+//int my_strlen(char *str)
+//{
+//	if (*str == 0)
+//		return 0;
+//	else
+//		return 1 + strlen(str + 1);
+//}
+//
+//int main()
+//{
+//	int len = my_strlen("girl");
+//	printf("len = %d\n", len);
+//	return 0;
+//}
+
+
+
