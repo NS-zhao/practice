47f31cde95af845fd6803fa97c1082bcb6080c53
NS-zhao <1783583968@qq.com>
2020-03-21 13:12:52 +0800

    java基本数据类型

---
+++ java/One.java
@@ -0,0 +1,145 @@
+/*public class One{
+    public static void main(String[] args){
+        int num = 10;
+        System.out.println(num);
+        
+    }
+}*/
+
+
+/*public class One{
+    public static void main(String[] args){
+        int num;
+        System.out.println(num);
+    }
+}*/
+
+
+
+/*public class One{
+    int num;
+    public static void main(String[] args){
+        
+        System.out.println(num);
+    }
+}*/
+
+
+/*public class One{
+    public static void main(String[] args){
+        
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+    }
+}*/
+
+
+
+/*public class One{
+    public static void main(String[] args){
+        int i = Integer.MAX_VALUE+1;
+        System.out.println(i);
+    }
+}*/
+
+
+
+/*public class One{
+    public static void main(String[] args){
+        int i = Integer.MIN_VALUE-1;
+        System.out.println(i);
+    }
+}*/
+
+
+
+/*public class One{
+    public static void main(String[] args){
+        
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+    }
+}*/
+
+
+
+/*public class One{
+    public static void main(String[] args){
+        int num1 = 1;
+        int num2 = 2;
+        System.out.println(num1/num2);
+    }
+}*/
+
+
+
+/*public class One{
+    public static void main(String[] args){
+        double num1 = 1;
+        double num2 = 2;
+        System.out.println(num1/num2);
+    }
+}*/
+
+
+
+/*public class One{
+    public static void main(String[] args){
+        double num1 = 1.1;
+        double num2 = 1.1;
+        System.out.println(num1*num2);
+    }
+}*/
+
+
+
+/*public class One{
+    public static void main(String[] args){
+        double num1 = 1.2;
+        double num2 = 1.2;
+        System.out.println(num1*num2);
+    }
+}*/
+
+
+
+/*public class One{
+    public static void main(String[] args){
+        BigDecimal b1 = new BigDecimal(String.valueOf(1.1));
+        BigDecimal b2 = new BigDecimal(String.valueOf(1.1));
+        double result = b1.multiply(b2).doubleValue();
+        System.out.println(result);
+    }
+}*/
+
+
+
+/*public class One{
+    public static void main(String[] args){
+       float f1 = 1.1f;
+       float f2 = 1.1f;
+        System.out.println(f1*f2);
+    }
+}*/
+
+
+
+/*public class One{
+    public static void main(String[] args){
+       char c = 'a';
+       char ch = '哈';
+        System.out.println(c);
+        System.out.println(ch);
+    }
+}*/
+
+
+
+/*public class One{
+    public static void main(String[] args){
+        int 变量1 = 10;
+        int 变量2 = 20;
+        int 计算结果 = 变量1 +变量2；
+        System.out.println(计算结果)；
+    }
+}*/
