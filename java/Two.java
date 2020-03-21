f50f66a7ed53c8677a1115e3ffa4e3d6c184b799
NS-zhao <1783583968@qq.com>
2020-03-21 22:45:16 +0800

    常量和变量

---
+++ java/Two.java
@@ -0,0 +1,99 @@
+/*public class Two{
+    public static void main(String[] args){
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println(b1);
+        System.out.println(b2);
+    }
+}*/
+
+
+/*public class Two{
+    public static void main(String[] args){
+       char c = '哈';
+       String name = "猪头"；
+        System.out.println(c);
+        System.out.println(name);
+    }
+}*/
+
+
+
+
+/*public class Two{
+    public static void main(String[] args){
+       boolean value = true;
+
+        System.out.println(value + 1);
+       
+    }
+}*/
+
+
+
+
+/*public class Two{
+    public static void main(String[] args){
+      String name = "\\猪头"；
+        System.out.println(name);
+       
+    }
+}*/
+
+
+
+/*public class Two{
+    public static void main(String[] args){
+      int num1 = 10;
+      int num2 = 20;
+      String result = "计算结果："+num1+num2;
+        System.out.println(result);
+       
+    }
+}*/
+
+
+
+/*public class Two{
+    public static void main(String[] args){
+        {
+            int i = 10;
+        }
+        System.out.println(i);
+       
+    }
+}*/
+
+
+
+/*public class Two{
+    public static void main(String[] args){
+        final int i = 0;
+        i = 5;
+        System.out.println(i);
+       
+    }
+}*/
+
+
+
+/*public class Two{
+    final int i;
+    public static void main(String[] args){
+
+        System.out.println(i);
+       
+    }
+}*/
+
+
+
+/*public class Two{
+    int i;
+    public static void main(String[] args){
+
+        System.out.println(i);
+       
+    }
+}*/
+
