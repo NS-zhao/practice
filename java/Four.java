563689c912a67f1368ac251b2130f75527f5676b
NS-zhao <1783583968@qq.com>
2020-03-23 22:15:57 +0800

    操作符

---
+++ java/Four.java
@@ -0,0 +1,170 @@
+/*public class Four{
+    public static void main(String[] args){
+        int a = 1;
+        int b = 2;
+        System.out.println(a/b);
+    }
+}*/
+
+
+
+
+/*public class Four{
+    public static void main(String[] args){
+        int a = 1;
+        int b = 0;
+        System.out.println(a/b);
+    }
+}*/
+
+
+
+
+/*public class Four{
+    public static void main(String[] args){
+        double a = 11.5;
+        int b = 2;
+        System.out.println(a%b);
+    }
+}*/
+
+
+
+/*public class Four{
+    public static void main(String[] args){
+        System.out.println(11.5%2);
+    }
+}*/
+
+
+
+/*public class Four{
+    public static void main(String[] args){
+        int a = 0;
+        //System.out.println(++a);
+        System.out.println(a++);
+    }
+}*/
+
+
+
+
+/*public class Four{
+    public static void main(String[] args){
+        int a = 10;
+        a+=1;
+        System.out.println(a);
+    }
+}*/
+
+
+
+/*public class Four{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a < b);
+        System.out.println(a > b);
+        System.out.println(a <= b);
+        System.out.println(a >= b);
+    }
+}*/
+
+
+
+/*public class Four{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        System.out.println(a < b && a < c);
+    }
+}*/
+
+
+
+/*public class Four{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        System.out.println(a < b||a > c);
+    }
+}*/
+
+
+/*public class Four{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        System.out.println(a > b||a > c);
+    }
+}*/
+
+
+
+/*public class Four{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        System.out.println(!(a > b));
+    }
+}*/
+
+
+
+/*public class Four{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        if(a < b||10/0 == 0){
+            System.out.println("hehe");
+        }else{
+            System.out.println("haha");
+        }
+    }
+}*/
+
+
+
+/*public class Four{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        if(a < b&&10/0 == 0){
+            System.out.println("hehe");
+        }else{
+            System.out.println("haha");
+        }
+    }
+}*/
+
+
+
+
+/*public class Four{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        if(a > b&&10/0 == 0){
+            System.out.println("hehe");
+        }else{
+            System.out.println("haha");
+        }
+    }
+}*/
+
+
+
+public class Four{
+    public static void main(String[] args){
+        System.out.println(10>20&10/0 == 0);
+        System.out.println(10<20|10/0 == 0);
+    }
+}
+
+
+
