9cfedb8544f8971776d0ae473aee034f4fe08ac5
NS-zhao <1783583968@qq.com>
2020-03-28 19:05:50 +0800

    while循环

---
+++ java/Nine.java
@@ -0,0 +1,65 @@
+/*public class Nine{
+    public static void main(String[] args){
+        int num = 1;
+        int sum = 0;
+        while(num<=5){
+            int tempResult = 1;
+            int i = 1;
+            while(i<=num){
+                tempResult*=i;
+                i++;
+
+            }
+            sum+=tempResult;
+            num++;
+        }
+        System.out.println("值为"+sum);
+        
+    }
+}*/
+
+
+
+/*public class Nine{
+    public static void main(String[] args){
+        int num = 1;
+        while(num<=10);{
+            System.out.println(num);
+            num++;
+        }
+    }
+}*/
+
+
+
+
+/*public class Nine{
+    public static void main(String[] args){
+        int num = 100;
+        while(num<=200){
+            if(num%3 == 0){
+                System.out.println(num);
+                break;
+                
+            }
+            num++;
+        }
+    }
+}*/
+
+
+
+
+/*public class Nine{
+    public static void main(String[] args){
+        int num = 100;
+        while(num<=200){
+            if(num%3 == 0){
+                System.out.println(num);
+                continue;
+                
+            }
+            num++;
+        }
+    }
+}*/
