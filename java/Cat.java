4435f46741116104e999bad051dd502f6b174800
NS-zhao <1783583968@qq.com>
2020-03-30 23:07:17 +0800

    类型转换

---
+++ java/Cat.java
@@ -0,0 +1,114 @@
+/*public class Cat{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        if(a>b&&a>c){
+            System.out.println("max:"+a);
+        }
+        if(b>a&&b>c){
+            System.out.println("max:"+b);
+        } else{
+            System.out.println("max:"+c);
+        }
+        
+    }
+}*/
+
+
+
+
+/*public class Cat{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        if(a<b&&a<c){
+            System.out.println("min:"+a);
+        }
+        if(b<a&&b<c){
+            System.out.println("min:"+b);
+        } 
+        if(c<a&&c<b){
+            System.out.println("min:"+c);
+        }
+    }
+}*/
+
+
+
+/*public class Cat{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        if(a>b&&a>c){
+            System.out.println("max:"+a);
+        }
+        if(b>a&&b>c){
+            System.out.println("max:"+b);
+        } else{
+            System.out.println("max:"+c);
+        }
+        if(a<b&&a<c){
+            System.out.println("min:"+a);
+        }
+        if(b<a&&b<c){
+            System.out.println("min:"+b);
+        }
+        if(c<a&&c<b){
+            System.out.println("min:"+c);
+        }
+    }
+}*/
+
+
+
+/*public class Cat{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int result = Math.max(c,Math.max(a,b));
+        System.out.println(result);
+    }
+}*/
+
+
+
+
+/*public class Cat{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int result = Math.min(c,Math.min(a,b));
+        System.out.println(result);
+    }
+}*/
+
+
+
+
+/*public class Cat{
+    public static void main(String[] args){
+        char c = 'a';
+        int a = c;
+        System.out.println(a);
+    }
+}*/
+
+
+
+
+/*public class Cat{
+    public static void main(String[] args){
+        char c = 'a';
+        int a = 97;
+        char b = a;
+        System.out.println(b);
+    }
+}*/
+
+
+
