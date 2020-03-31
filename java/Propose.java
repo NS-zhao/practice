9aa2545a9d2b4e78fb2435482e77f1cacda5a990
NS-zhao <1783583968@qq.com>
2020-03-31 21:55:33 +0800

    类型转换

---
+++ java/Propose.java
@@ -0,0 +1,145 @@
+/*public class Propose{
+    public static void main(String[] args){
+        char c = 'a';
+        int a = c;
+        System.out.println(a);
+    }
+}*/
+
+
+
+/*public class Propose{
+    public static void main(String[] args){
+        char c = 'a';
+        int a = 97;
+        char b = a;
+        System.out.println(b);
+    }
+}*/
+
+
+
+/*public class Propose{
+    public static void main(String[] args){
+        int a = 120;
+        byte c = a;
+        System.out.println(c);
+    }
+}*/
+
+
+
+/*public class Propose{
+    public static void main(String[] args){
+        long a = Integer.MAX_VALUE+1;
+        int b = (int)a;
+        System.out.println(b);
+    }
+}*/
+
+
+
+
+/*public class Propose{
+    public static void main(String[] args){
+        double a = 10.5;
+        int b = (int) a;
+        System.out.println(b);
+    }
+}*/
+
+
+
+/*public class Propose{
+    public static void main(String[] args){
+        int a = 10;
+        boolean b = false;
+        b = (boolean)a;
+        System.out.println(b);
+    }
+}*/
+
+
+
+/*public class Propose{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        long c = a+b;
+        System.out.println(c);
+        //int d = a+c;
+        int d = (int)(a+c);
+        System.out.println(d);
+    }
+}*/
+
+
+
+
+/*public class Propose{
+    public static void main(String[] args){
+        char a = 'A';
+        int b = a+32;
+        char c = (char)b;
+        System.out.println(b);
+        System.out.println(c);
+    }
+}*/
+
+
+
+/*public class Propose{
+    public static void main(String[] args){
+        byte a = 10;
+        byte b = 20;
+        byte c = a+b;
+        System.out.println(c);
+    }
+}*/
+
+
+
+/*public class Propose{
+    public static void main(String[] args){
+        byte a = 10;
+        byte b = 20;
+        byte c = (byte)(a+b);
+        System.out.println(c);
+    }
+}*/
+
+
+
+/*public class Propose{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        String result = "计算结果："+a+b;
+        System.out.println(result);
+    }
+}*/
+
+
+
+/*
+public class Propose{
+    public static void main(String[] args){
+        int a = 10;
+        String str = String.valueOf(a);
+        System.out.println(str);
+        int b = str+10;
+
+    }
+}*/
+
+
+
+
+public class Propose{
+    public static void main(String[] args){
+        String str = "123";
+        int a = Integer.parseInt(str);
+        int b = a+10;
+        System.out.println(a);
+    }
+}
