18290c124bd8caba8d4f076fbd406921ae268836
NS-zhao <1783583968@qq.com>
2020-04-03 22:54:59 +0800

    方法重载

---
+++ java/Dim.java
@@ -0,0 +1,164 @@
+/*public class Dim{
+    public static void main(String[] args){
+        test(1);
+        test(2);
+        test(3);
+        test(4);
+    }
+
+    public static void test(int num){
+        if(num == 2){
+            return;
+        }
+        System.out.println(num);
+    }
+}*/
+
+
+
+
+/*public class Dim{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        print(a,b);
+    }
+
+    public static void print(int x,int y){
+        System.out.println("x = "+x+",y = "+y);
+    }
+}*/
+
+
+
+/*public class Dim{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int ret = add(a,b);
+        System.out.println("ret = "+ret);
+        
+    }
+
+    public static int add(int x,int y){
+        return x+y;
+    }
+}*/
+
+
+
+
+/*public class Dim{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int ret = add(a,b);
+        System.out.println("ret = "+ret);
+        
+        double a1 = 10.0;
+        double b1 = 20.0;
+        double ret1 = add(a1,b1);
+        System.out.println("ret1 = "+ret1);
+    }
+
+    public static int add(int x,int y){
+        return x+y;
+    }
+}*/
+
+
+
+
+/*public class Dim{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int ret = add(a,b);
+        System.out.println("ret = "+ret);
+        
+        double a1 = 10.0;
+        double b1 = 20.0;
+        double ret1 = addDouble(a1,b1);
+        System.out.println("ret1 = "+ret1);
+    }
+
+    public static int add(int x,int y){
+        return x+y;
+    }
+
+    public static double addDouble(double x,double y){
+        return x+y;
+    }
+}*/
+
+
+
+
+/*public class Dim{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int ret = addInt(a,b);
+        System.out.println("ret = "+ret);
+        
+        double a1 = 10.0;
+        double b1 = 20.0;
+        double ret1 = addDouble(a1,b1);
+        System.out.println("ret1 = "+ret1);
+
+
+        double a2 = 10.5;
+        double b2 = 20.5;
+        double c2 = 30.5;
+        double ret2 = addDouble(a2,b2,c2);
+        System.out.println("ret2 = "+ret2);
+    }
+
+    public static int addInt(int x,int y){
+        return x+y;
+    }
+
+    public static double addDouble(double x,double y){
+        return x+y;
+    }
+
+    public static double addDouble(double x,double y,double z){
+        return x+y+z;
+    }
+}*/
+
+
+
+
+public class Dim{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int ret = add(a,b);
+        System.out.println("ret = "+ret);
+        
+        double a1 = 10.0;
+        double b1 = 20.0;
+        double ret1 = add(a1,b1);
+        System.out.println("ret1 = "+ret1);
+
+
+        double a2 = 10.5;
+        double b2 = 20.5;
+        double c2 = 30.5;
+        double ret2 = add(a2,b2,c2);
+        System.out.println("ret2 = "+ret2);
+    }
+
+    public static int add(int x,int y){
+        return x+y;
+    }
+
+    public static double add(double x,double y){
+        return x+y;
+    }
+
+    public static double add(double x,double y,double z){
+        return x+y+z;
+    }
+}
