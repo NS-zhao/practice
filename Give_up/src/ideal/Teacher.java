ff53e7ce1233fcf02c1971d232086269990baa57
NS-zhao <1783583968@qq.com>
2020-04-11 20:37:36 +0800

    Java基础

---
+++ Give_up/src/ideal/Teacher.java
@@ -0,0 +1,120 @@
+package ideal;
+
+//public class Rectangle {
+//    double width;
+//    double height;
+//    double getArea(){
+//        return width*height;
+//    }
+//
+//        }
+//class Test{
+//    public static void main(String[] args) {
+//        Rectangle r;
+//        r = new Rectangle();
+//        r.width = 19.18;
+//        r.height = 20;
+//        double area = r.getArea();
+//        System.out.println("矩形的面积："+area);
+//    }
+//}
+
+
+
+//public class Rectangle{
+//    public static void main(String[] args) {
+//        int sum = 0;
+//        for(int i = 0;i<=100;i++){
+//            sum+=i;
+//        }
+//        System.out.println(sum);
+//    }
+//}
+
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+//public class Rectangle{
+//    public static void main(String[] args) {
+//        System.out.println("你好，只需要编译我");
+//        A a = new A();
+//        a.fA();
+//        B b = new B();
+//        b.fB();
+//
+//    }
+//}
+//class A{
+//    void fA(){
+//        System.out.println("I am A");
+//    }
+//}
+//class B{
+//    void fB(){
+//        System.out.println("I am B");
+//    }
+//}
+
+
+//public class Teacher{
+//    double add(double a,double b){
+//        return a+b;
+//    }
+//    double sub(double a,double b){
+//        return a-b;
+//    }
+//}
+//class Student{
+//    public static void speak(){
+//        System.out.println("老师好");
+//    }
+//}
+//class MainClass{
+//    public static void main(String[] args) {
+//        Teacher zhao = new Teacher();
+//        System.out.println(zhao.add(12,236));
+//        System.out.println(zhao.sub(234,120));
+//        Student min = new Student();
+//        min.speak();
+//    }
+//}
+
+
+
+//public class Teacher{
+//    public static void main(String[] args) {
+//        char chinaWord = '好';
+//        int position = 20320;
+//        System.out.println("汉字"+chinaWord+"的位置"+(int)chinaWord);
+//        System.out.println(position+"位置上的字符是："+(char)position);
+//        position = 21319;
+//        System.out.println(position+"位置上的字符是："+(char)position);
+//    }
+//}
+
+
+//public class Teacher{
+//    int age;
+//    float weight;
+//    void speak(){
+//        System.out.println("我的年龄"+age+"岁");
+//        System.out.println("我的体重"+weight+"公斤");
+//    }
+//
+//    public static void main(String[] args) {
+//        Teacher zhang,zhou;
+//        zhang = new Teacher();
+//        zhang.age = 18;
+//        zhang.weight = 55.5f;
+//        zhang.speak();
+//        zhou = new Teacher();
+//        zhou.age = 19;
+//        zhou.weight = 70f;
+//        zhou.speak();
+//    }
+//
+//}
+
+
+
