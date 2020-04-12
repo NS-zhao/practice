a947808d9464861b7e543d59af9cdb8090457d32
NS-zhao <1783583968@qq.com>
2020-04-12 19:19:54 +0800

    数据类型

---
+++ Give_up/src/ideal/A.java
@@ -0,0 +1,96 @@
+package ideal;
+
+//import java.util.Scanner;
+//public class A {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        double sum = 0;
+//        int i = 0;
+//        while(sc.hasNextDouble()){
+//            double x = sc.nextDouble();
+//            i++;
+//            sum+=x;
+//        }
+//        System.out.printf("%d个数的和是%f\n",i,sum);
+//        System.out.printf("%d个数的平均值是%f\n",i,sum/i);
+//
+//    }
+//}
+
+
+
+//public class A{
+//    public static void main(String[] args) {
+//        for (int i = 20302; i < 20322; i++) {
+//            System.out.println((char)i);
+//        }
+//    }
+//}
+
+
+
+//public class A{
+//    public static void main(String[] args) {
+//        int x = 234,y = 432;
+//        System.out.println(x+"<"+(2*x));
+//        System.out.print("我输出结果后不回车");
+//        System.out.println("我输出结果后自动回车到下一行");
+//        System.out.println("x+y = "+(x+y));
+//    }
+//}
+
+
+
+//public class A{
+//    public static void main(String[] args) {
+//        char start = 'A';
+//        char end = 'Z';
+//        for (char i = start; i <= end ; i++) {
+//            System.out.print(" "+i);
+//
+//        }
+//    }
+//}
+
+
+//import java.util.Scanner;
+//public class A{
+//    int age;
+//    float weight;
+//    void speak(){
+//        System.out.println("我的年龄"+age+"岁");
+//        System.out.println("我的体重"+weight+"公斤");
+//    }
+//    public static void main(String[] args) {
+//        A zhangsan = new A();
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("输入年龄，回车确认");
+//        zhangsan.age = sc.nextInt();
+//        System.out.println("输入体重，回车确认");
+//        zhangsan.weight = sc.nextFloat();
+//        zhangsan.speak();
+//    }
+//}
+
+
+
+//import java.util.Scanner;
+//public class A{
+//    int age;
+//    float weight;
+//    void speak(){
+//        System.out.println("我的年龄"+age+"岁");
+//        System.out.println("我的体重"+weight+"公斤");
+//    }
+//    public static void main(String[] args) {
+//        A zhangsan = new A();
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("输入年龄，回车确认");
+//        zhangsan.age = sc.nextInt();
+//        System.out.println("输入体重，回车确认");
+//        zhangsan.weight = sc.nextDouble();
+//        zhangsan.speak();
+//    }
+//}
+
+
