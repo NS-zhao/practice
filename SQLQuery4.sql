7d0ff81748c6be49cb035b045d3364dd76ca7cd8
NS-zhao <1783583968@qq.com>
2020-04-13 23:31:12 +0800

    创建表

---
+++ SQLQuery4.sql
@@ -0,0 +1,8 @@
+USE inventory
+GO
+CREATE TABLE store
+(stno char(3) PRIMARY KEY,
+address nvarchar(30) NOT NULL,
+telephone varchar(11) check(telephone like '[0-9] [0-9] [0-9] [0-9] [0-9] [0-9] [0-9] [0-9] [0-9] [0-9] [0-9]' ),
+capacity smallint,
+)
