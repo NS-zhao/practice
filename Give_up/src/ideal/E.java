544bfaa46ab0ccd0d9bbbcf9545af8baabed905d
NS-zhao <1783583968@qq.com>
2020-04-21 22:54:59 +0800

    类

---
+++ Give_up/src/ideal/E.java
@@ -0,0 +1,119 @@
+package ideal;
+
+//public class E {
+//        int x = 10,y;
+//        int f() {
+//            int x = 5;
+//            y = x + x;
+//            return y;
+//        }
+//    public static void main(String[] args) {
+//        E e = new E();
+//        int z = e.f();
+//        System.out.println(z);
+//    }
+//}
+
+
+
+//public class E{
+//    float height,weight;
+//    String head,ear,hand,foot,mouth;
+//    void speak(String s){
+//        System.out.println(s);
+//    }
+//
+//    public static void main(String[] args) {
+//        E e;
+//        e = new E();
+//    }
+//}
+
+
+
+//public class Point{
+//    int x,y;
+//    Point(int a,int b){
+//        x = a;
+//        y = b;
+//    }
+//
+//    public static void main(String[] args) {
+//        Point p1,p2;
+//        p1 = new Point(10,20);
+//        p2 = new Point(23,35);
+//    }
+//}
+
+
+
+//public class E{
+//    float above;
+//    float bottom;
+//    float height;
+//    float area;
+//    float computerArea(){
+//        area = (above+bottom)*height/2.0f;
+//        return area;
+//    }
+//    void setHeight(float h){
+//        height = h;
+//    }
+//}
+
+
+
+//public class E{
+//    int getSum(int n){
+//        int sum = 0;
+//        for (int i = 1; i <= n; i++) {
+//            sum = sum+i;
+//        }
+//        return sum;
+//    }
+//
+//    public static void main(String[] args) {
+//        E e = new E();
+//        int s = e.getSum(8);
+//        System.out.println(s);
+//    }
+
+
+
+
+
+//public class E{
+//    int m = 10,sum = 0;
+//    void f(){
+//        if(m>9){
+//            int z = 10;
+//            z = 2*m+z;
+//        }
+//        for (int i = 0; i < m; i++) {
+//            sum = sum+i;
+//        }
+//        m = sum;
+//        z = i+sum;
+//    }
+//}
+
+
+
+//public class E{
+//    int x = 10;
+//    int y;
+//    int  f(){
+//        int x = 5;
+//        y = x+this.x;
+//        return y;
+//    }
+//
+//    public static void main(String[] args) {
+//        E e = new E();
+//        int z = e.f();
+//        System.out.println(z);
+//    }
+//}
+
+
+
