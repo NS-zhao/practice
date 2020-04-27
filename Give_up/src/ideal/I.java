26643ebf089a8317580cfc860467ade13bc18676
NS-zhao <1783583968@qq.com>
2020-04-27 23:16:56 +0800

    继承，隐藏，重写

--- Give_up/src/ideal/I.java
+++ Give_up/src/ideal/I.java
@@ -123,23 +123,23 @@ package ideal;
 
 
 
-class People{
-    int leg;
-    int hand;
-    String name;
-    People(String s){
-        name = s;
-        this.init();
-    }
-
-    void init(){
-        leg = 2;
-        hand = 2;
-        System.out.println(name+"有"+hand+"只手"+leg+"条腿");
-    }
-}
-public class I{
-    public static void main(String[] args) {
-        People boshi = new People("布什");
-    }
-}
+//class People{
+//    int leg;
+//    int hand;
+//    String name;
+//    People(String s){
+//        name = s;
+//        this.init();
+//    }
+//
+//    void init(){
+//        leg = 2;
+//        hand = 2;
+//        System.out.println(name+"有"+hand+"只手"+leg+"条腿");
+//    }
+//}
+//public class I{
+//    public static void main(String[] args) {
+//        People boshi = new People("布什");
+//    }
+//}
