987a302278a72f2dc7bec6a1fbd98a60e6c1b1c1
NS-zhao <1783583968@qq.com>
2020-05-12 21:04:48 +0800

    Date

---
+++ 2020/C.java
@@ -0,0 +1,26 @@
+package ideal;
+
+//import java.util.Date;
+//import java.text.SimpleDateFormat;
+//
+//public class C {
+//    public static void main(String[] args) {
+//        Date nowTime = new Date();
+//        System.out.println(nowTime);
+//        String pattern = "yyy-MM-dd";
+//        SimpleDateFormat SDF = new SimpleDateFormat(pattern);
+//        String timePattern = SDF.format(nowTime);
+//        System.out.println(timePattern);
+//        pattern = "G yyy年MMMd日E HH时mm分ss秒z";
+//        SDF = new SimpleDateFormat("G yyy年MMMd日E HH时mm分ss秒z");
+//        timePattern = SDF.format(nowTime);
+//        System.out.println(timePattern);
+//        long time = System.currentTimeMillis();
+//        System.out.println("现在是公元后："+time+"毫秒");
+//    }
+//}
+
+
+
+
+
