f81aebd3e9a409efead0c393bf4e4a8621328067
NS-zhao <1783583968@qq.com>
2020-07-17 22:04:16 +0800

    简单的类

---
+++ Java/src/R/A.java
@@ -0,0 +1,139 @@
+package R;
+
+//public class A {
+//    public static void main (String[] args) {
+//        System.out.println("你好，很高兴学习java");
+//        Student zhao = new Student();
+//        zhao.speak();
+//    }
+//}
+//class Student{
+//    void speak(){
+//        System.out.println("We are students");
+//    }
+//}
+
+
+
+//public class A{
+//    public static void main (String[] args) {
+//        double ridius;
+//        double area;
+//        ridius = 123.86;
+//        area = 3.14*ridius*ridius;
+//        System.out.println(area);
+//    }
+//}
+
+
+
+//class Circle{
+//    double radius;
+//    double getArea(){
+//        double area = 3.14*radius*radius;
+//        return area;
+//    }
+//}
+//
+//public class A{
+//    public static void main (String[] args) {
+//        Circle circleOne,circleTwo;
+//        circleOne = new Circle();
+//        circleTwo = new Circle();
+//        circleOne.radius = 120.85;
+//        circleTwo.radius = 145.25;
+//        double area = circleOne.getArea();
+//        System.out.println("circleOne的面积："+area);
+//        area = circleTwo.getArea();
+//        System.out.println("circleTwo的面积："+area);
+//    }
+//}
+
+
+//class Rect{
+//    double width;
+//    double height;
+//    double getArea(){
+//        double area = width*height;
+//        return area;
+//    }
+//}
+//
+//class Lader{
+//    double shangdi;
+//    double xiadi;
+//    double height;
+//    double getArea(){
+//        double area = (shangdi+xiadi)*height;
+//        return area;
+//    }
+//}
+//
+//public class A{
+//    public static void main (String[] args) {
+//        Rect rect = new Rect();
+//        rect.height = 25.18;
+//        rect.width = 125.05;
+//        double area = rect.getArea();
+//        System.out.println("矩形的面积:"+area);
+//        Lader lader = new Lader();
+//        lader.shangdi = 25.36;
+//        lader.xiadi = 52.36;
+//        lader.height = 18.12;
+//        area = lader.getArea();
+//        System.out.println("梯形的面积："+area);
+//    }
+//}
+
+
+
+//public class A{
+//    public static void main (String[] args) {
+//        System.out.println("你好，只需编译我");
+//        fa a = new fa();
+//        a.fA();
+//        fb b = new fb();
+//        b.fB();
+//    }
+//}
+//
+//class fa{
+//    void fA(){
+//        System.out.println("I am A");
+//    }
+//}
+//
+//class fb{
+//    void fB(){
+//        System.out.println("I am B");
+//    }
+//}
+
+
+
+//class Teacher{
+//    double add(double a,double b){
+//        return a+b;
+//    }
+//    double sub(double a,double b){
+//        return a-b;
+//    }
+//}
+//
+//class Student{
+//    void speak(){
+//        System.out.println("老师好");
+//    }
+//}
+//
+//public class A{
+//    public static void main (String[] args) {
+//        Teacher zhang = new Teacher();
+//        double he = zhang.add(12,236);
+//        System.out.println("12和236的和为:"+he);
+//        double jian = zhang.sub(234,120);
+//        System.out.println("234和120的差为："+jian);
+//        Student zhao = new Student();
+//        zhao.speak();
+//    }
+//}
