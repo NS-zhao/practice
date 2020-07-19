7bc9440cf67ee3a3ba7427a0f0b9b69a13c852f0
NS-zhao <1783583968@qq.com>
2020-07-19 22:03:53 +0800

    Scanner

---
+++ Java/src/R/C.java
@@ -0,0 +1,79 @@
+package R;
+
+//import java.util.Scanner;
+//
+//public class C {
+//    public static void main (String[] args) {
+//        Scanner reader = new Scanner(System.in);
+//        double sum = 0;
+//        int m = 0;
+//        while(reader.hasNextDouble()){
+//            double x = reader.nextDouble();
+//            m = m+1;
+//            sum = sum+x;
+//
+//        }
+//        System.out.println(m+"个数的和为："+sum);
+//        System.out.println(m+"个数的平均值为"+sum/m);
+//    }
+//}
+
+
+
+//public class C{
+//    public static void main (String[] args) {
+//        for (int i = 20302; i <= 20322; i++) {
+//            System.out.println((char)i);
+//        }
+//    }
+//}
+
+
+
+//public class C{
+//    public static void main (String[] args) {
+//        int x = 234,y = 432;
+//        System.out.println(x+"<"+(2*x));
+//        System.out.print("我输出结果后不回车");
+//        System.out.println("我输出结果后自动回车到下一行");
+//        System.out.println("x+y = "+(x+y));
+//    }
+//}
+
+
+
+//public class C{
+//    public static void main (String[] args) {
+//        for (char i = 'A'; i <= 'Z'; i++) {
+//            System.out.print(i+" ");
+//        }
+//    }
+//}
+
+
+
+//import java.util.Scanner;
+//
+//class People{
+//    int age;
+//    float weight;
+//    void speak(){
+//        System.out.println("我的年龄："+age+"岁");
+//        System.out.println("我的体重："+weight+"公斤");
+//    }
+//}
+//
+//public class C{
+//    public static void main(String[] args){
+//        People Ruanxiangge = new People();
+//        Scanner reader = new Scanner(System.in);
+//        System.out.println("输入年龄，回车确认");
+//        Ruanxiangge.age = reader.nextInt();
+//        System.out.println("输入体重，回车确认");
+//        Ruanxiangge.weight = reader.nextInt();
+//        Ruanxiangge.speak();
+//    }
+//}
+
+
+
