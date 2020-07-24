f2ec4e504644fa22185c16cfbe68c02a4e5bbeff
NS-zhao <1783583968@qq.com>
2020-07-24 22:35:29 +0800

    类与方法

---
+++ Java/src/R/H.java
@@ -0,0 +1,106 @@
+package R;
+
+//class Lader{
+//    float above;
+//    float bottom;
+//    float height;
+//    float area;
+//    float computerArea(){
+//        area = (above+bottom)*height/2.0f;
+//        return area;
+//    }
+//    void setHeight(float h){
+//        height = h;
+//    }
+//}
+
+
+
+    //class Factory{
+    //    float a[];
+    //    Workman zhang;
+    //}
+    //
+    //class Workman{
+    //    double x;
+    //}
+
+
+//class A{
+//    int getSum(int n){
+//        int sum = 0;
+//        for (int i = 1; i <= n; i++) {
+//            sum = sum+i;
+//        }
+//    }
+//    return sum;
+//}
+
+
+//class A{
+//    int m = 10;
+//    int sum = 0;
+//    void f(){
+//        if(m>9){
+//            int z = 10;
+//            z = 2*m+z;
+//        }
+//        for (int i = 0; i < m; i++) {
+//            sum = sum+i;
+//        }
+//        m = sum;
+//        z = i+sum;
+//    }
+//}
+
+
+
+//class A{
+//    int x = 10;
+//    int y;
+//    void f(){
+//        int x = 5;
+//        y = x+x;
+//        System.out.println(y);
+//    }
+//}
+//
+//public class H{
+//    public static void main (String[] args) {
+//        A a = new A();
+//        a.f();
+//    }
+//}
+
+
+
+//class A{
+//    int x = 10;
+//    int y;
+//    void f(){
+//        int x = 5;
+//        y = x+this.x;
+//        System.out.println(y);
+//    }
+//}
+//
+//public class H{
+//    public static void main (String[] args) {
+//        A a = new A();
+//        a.f();
+//    }
+//}
+
+
+
+//class Point{
+//    int x,y;
+//    Point(){
+//        x = 1;
+//        y = 1;
+//    }
+//    Point(int a,int b){
+//        x = a;
+//        y = b;
+//    }
+//}
