6f83e2755238a7fdd26f2e02a0c5b5de03676d09
NS-zhao <1783583968@qq.com>
2020-10-12 23:53:59 +0800

    数组

---
+++ C语言/test_6/test_6/test.c
@@ -0,0 +1,273 @@
+#include<stdio.h>
+
+//int fac(int n)
+//{
+//	if (n <= 1)
+//		return 1;
+//	else
+//		return n*fac(n - 1);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = fac(n);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+
+//int fib(int n)
+//{
+//	if (n <= 2)
+//		return 1;
+//	else
+//		return fib(n - 1) + fib(n - 2);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = fib(n);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+
+//int count = 0;
+//int fib(int n)
+//{
+//	if (n == 3)
+//		count++;
+//	if (n <= 2)
+//		return 1;
+//	else
+//		return fib(n - 1) + fib(n - 2);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = fib(n);
+//	printf("ret = %d\n", ret);
+//	printf("count = %d\n", count);
+//	return 0;
+//}
+
+
+
+//int fac(int n)
+//{
+//	int ret = 1;
+//	while (n > 1)
+//	{
+//		ret = ret*n;
+//		n--;
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = fac(n);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+
+//int fib(int n)
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while (n > 2)
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = fib(n);
+//	printf("ret = %d\n", ret);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//	}
+//	for (i = 0; i < 10; ++i)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < sizeof(arr) / sizeof(arr[0]); ++i)
+//	{
+//		printf("&arr[%d] = %p\n", i, &arr[i]);
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("&arr[%d] = %p\n", i, &arr[i]);
+//
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int *p = &arr[0];
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("&arr[%d] = %p<==>%p\n", i, &arr[i], p + i);
+//		printf("%d ", *(p + i));
+//
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int arr[3][4] = { { 1, 2 }, { 3, 4 } };
+//	printf("%d\n", arr[1][1]);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int arr[3][4] = { 0, 2, 3, 4, 5 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int arr[3][4] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 4; j++)
+//		{
+//			arr[i][j] = i * 4 + j;
+//		}
+//	}
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int arr[3][4];
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("&arr[%d][%d] = %p\n", i, j, &arr[i][j]);
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+//void bubble_sort(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j <= sz - 1 - i; j++)
+//		{
+//			if (arr[j]>arr[j + 1])
+//			{
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//			}
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bubble_sort(arr, sz);
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
