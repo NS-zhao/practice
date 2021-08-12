1e7e3a5273ed7f85aa082d2a83b330b7fac596f2
NS-zhao <1783583968@qq.com>
2021-08-12 23:14:27 +0800

    变量

--- web/js/PC端购物车/js/Cart-1.js
+++ web/js/PC端购物车/js/Cart-1.js
@@ -1,6 +1,59 @@
 /**
  * Created by zjm on 2021/8/10.
  */
+var ckall=null;
+var cklist=null;
+var fanck=null;
+var item=null;
 window.onload=function(){
+    ckall=document.querySelector(".ckall");
+    cklist=document.querySelectorAll(".cklist");
+    fanck=document.querySelector(".fanck");
+    item=document.querySelectorAll(".item");
 
+    //全选功能
+    ckall.onclick= function(){
+        //获取全选按钮的check属性，根据值进行全选和不选
+        var ckcheck=this.checked;
+        checkStatus(ckcheck)
+    }
+    //反选
+    fanck.onclick=function(){
+        checkStatus("fan");
+    }
+    //cklist自身的事件
+    addcklistHandle();
+}
+//封装方法实现全选和反选功能
+function checkStatus(status){
+    var isAll=true;
+
+    for(var i=0;i<cklist.length;i++){
+        if(status!=undefined){
+            //检测status变量的类型
+            cklist[i].checked=typeof status=="boolean"?status:!cklist[i].checked;
+            if(cklist[i].checked){
+                item[i].classList.add('ck')
+            }
+        }
+        if(!cklist[i].checked){
+            isAll=false;
+        }
+    }
+    if(!isAll){
+        ckall.checked=false;
+    }
+    else{
+        ckall.checked=true;
+    }
+}
+//给cklist添加事件的方法
+function addcklistHandle(){
+    for(var i=0;i<cklist.length;i++){
+        cklist[i].onclick=function(){
+            checkStatus();
+            //点击单个选择，当前行变色
+
+        }
+    }
 }
