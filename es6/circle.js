546a8b403dc7e4868554ce3d18d4c7a20783df90
NS-zhao <1783583968@qq.com>
2022-03-02 23:39:05 +0800

    Module的加载实现

---
+++ es6/circle.js
@@ -0,0 +1,9 @@
+/**
+ * Created by zjm on 2022/1/23.
+ */
+export function area(radius){
+    return Math.PI*radius*radius;
+}
+export function circumference(radius){
+    return 2*Math.PI*radius;
+}
